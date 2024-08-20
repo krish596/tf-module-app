25488a9a36e03f7213b431cd0a3604a829860326
Chimtamreddy <kr7348202@gmail.com>
2024-08-20 21:29:56 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -107,7 +107,8 @@ resource "aws_lb_listener_rule" "main" {
 }
 
 resource "aws_lb_target_group" "public" {
-  name     = "${local.name_prefix}-public"
+  count = var.component == "frontend" ? 1 : 0
+    name     = "${local.name_prefix}-public"
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
