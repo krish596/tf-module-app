9591eb9d246651e172ce80dbe58f99da6a717569
Chimtamreddy <kr7348202@gmail.com>
2024-08-20 21:40:48 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -115,8 +115,8 @@ resource "aws_lb_target_group" "public" {
 }
 
 resource "aws_lb_target_group_attachment" "public" {
-  count = length(data.dns_a_record_set.private_alb.addrs)
+  count = length(tolist(data.dns_a_record_set.private_alb.addrs)) ? 1 : 0
   target_group_arn = aws_lb_target_group.public[0].arn
-  target_id        = element(data.dns_a_record_set.private_alb.addrs, count.index)
+  target_id        = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
   port             = var.port
 }
