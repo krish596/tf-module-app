e050d0b4a2704646908f2c58836ded8bce62d22d
Chimtamreddy <kr7348202@gmail.com>
2024-08-20 19:27:10 +0530

    Terraform

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-# locals {
-#   name_prefix = "${var.env}-${var.component}"
-#   tags = merge(var.tags, {tf-module-name = "component"}, {env = var.env})
-# }
+locals {
+  name_prefix = "${var.env}-${var.component}"
+  tags = merge(var.tags, {tf-module-name = "component"}, {env = var.env})
+}
--- vars.tf
+++ vars.tf
@@ -1,18 +1,16 @@
-# variable "tags" {}
-# variable "env" {}
-# variable "component" {}
-# variable "port" {}
-# variable "subnet_ids" {}
-# variable "vpc_id" {}
-# variable "sg_ingress_cidr" {}
-# variable "ssh_ingress_cidr" {}
-# variable "zone_id" {}
-# variable "instance_type" {}
-# variable "desired_capacity" {}
-# variable "max_size" {}
-# variable "min_size" {}
-# variable "alb_name" {}
-# variable "listener" {}
-# variable "lb_priority" {}
-#
-#
+variable "tags" {}
+variable "env" {}
+variable "component" {}
+variable "port" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "ssh_ingress_cidr" {}
+variable "zone_id" {}
+variable "instance_type" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+
+
+
