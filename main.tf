6635b9ede07595713b4e305fcbccd81179cc5206
Chimtamreddy <kr7348202@gmail.com>
2024-08-21 19:42:45 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ resource "aws_launch_template" "main" {
   user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
       component = var.component
+      env       = var.env
     }))
 
 
