43d80576906a4d56b20d6df98c64541eefd6d4e5
Chimtamreddy <kr7348202@gmail.com>
2024-08-22 21:37:17 +0530

    Terraform

--- locals.tf
+++ locals.tf
@@ -3,5 +3,5 @@ locals {
   tags = merge(var.tags, {tf-module-name = "component"}, {env = var.env})
 
   parameters =concat(var.parameters, [var.component])
-  policy_resources =[ for i in local.parameters: "arn:aws:ssm:us-east-1:014498634764:parameter/${i}.${var.env}.*"]
+  policy_resources =[for i in local.parameters: "arn:aws:ssm:us-east-1:014498634764:parameter/${i}.${var.env}.*"]
 }
