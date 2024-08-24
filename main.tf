40520cdf023e53a504af95a8e6a9e681e0ec6f59
Chimtamreddy <kr7348202@gmail.com>
2024-08-24 15:30:17 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -24,14 +24,7 @@ resource "aws_security_group" "main" {
 
   }
 
-  ingress {
-    description = "PROMETHEUS"
-    from_port        = 9100
-    to_port          = 9100
-    protocol         = "tcp"
-    cidr_blocks      = var.monitoring_ingress_cidr
 
-  }
 
   egress {
     from_port        = 0
