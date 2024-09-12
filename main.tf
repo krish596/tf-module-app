abc3ed4597ca16bb0e424bec0778a2558b6604dd
Chimtamreddy <kr7348202@gmail.com>
2024-09-12 08:24:51 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -64,6 +64,7 @@ resource "aws_iam_policy" "main" {
         "Sid": "VisualEditor0",
         "Effect": "Allow",
         "Action": [
+          "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
