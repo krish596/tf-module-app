e6ff5b3da03498285f3d9047e49c25d871a00af3
Chimtamreddy <kr7348202@gmail.com>
2024-09-14 21:13:03 +0530

    ami

--- main.tf
+++ main.tf
@@ -41,16 +41,6 @@ resource "aws_security_group" "main" {
   }
 }
 
-resource "aws_security_group_rule" "nginx_exporter" {
-  count             = var.component == "frontend" ? 1 : 0
-  type              = "ingress"
-  from_port         = 9113
-  to_port           = 9113
-  protocol          = "tcp"
-  cidr_blocks       = var.monitoring_ingress_cidr
-  security_group_id = aws_security_group.main.id
-  description       = "Nginx Prometheus Exporter"
-}
 
 resource "aws_iam_policy" "main" {
   name        = "${local.name_prefix}-policy"
@@ -265,3 +255,14 @@ resource "aws_lb_listener_rule" "public" {
   }
 }
 
+
+resource "aws_security_group_rule" "nginx_exporter" {
+  count             = var.component == "frontend" ? 1 : 0
+  type              = "ingress"
+  from_port         = 9113
+  to_port           = 9113
+  protocol          = "tcp"
+  cidr_blocks       = var.monitoring_ingress_cidr
+  security_group_id = aws_security_group.main.id
+  description       = "Nginx Prometheus Exporter"
+}
