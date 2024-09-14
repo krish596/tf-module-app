27030c739d39d141c5b714daaf8b01a7f19ab880
Chimtamreddy <kr7348202@gmail.com>
2024-09-14 22:17:43 +0530

    ami

--- main.tf
+++ main.tf
@@ -53,17 +53,7 @@ resource "aws_security_group_rule" "nginx_exporter" {
   description       = "Nginx Prometheus Exporter"
 }
 
-resource "aws_security_group_rule" "nginx_exporter" {
 
-  count             = var.component == "frontend" ? 1 : 0
-  type              = "ingress"
-  from_port         = 9113
-  to_port           = 9113
-  protocol          = "tcp"
-  cidr_blocks       = "${ var.monitoring_ingress_cidr }"
-  security_group_id = "${aws_security_group.main.id}"
-  description       = "Nginx Prometheus Exporter"
-}
 
 
 resource "aws_iam_policy" "main" {
